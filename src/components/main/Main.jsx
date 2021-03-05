edb510b6f3f18dab77de053dd3007b4dc19a0c77
amrho1 <amirhosseinsalighedar@gmail.com>
2021-03-05 18:55:56 +0330

    finishing the dropdown for category

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,5 @@
 import "./Main.css"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Slider from "../slider/Slider";
 import { Switch, Route } from "react-router-dom";
 import Shop from "../shop/Shop";
@@ -7,17 +7,26 @@ import DropDown from "../shop/drop-down/DropDown";
 
 
 export default function Main() {
+  const [category, setCategory] = useState();
+
+  const changeCategory = (value) => {
+    setCategory(value)
+  }
+
+
   return (
     <main className="main">
       <Switch>
         <Route path="/" exact>
           <Slider />
         </Route>
+
         <Route path="/shop">
+
           <div className="top-of-shop">
-            <DropDown />
+            <DropDown changeCategory={changeCategory} />
           </div>
-          <Shop />
+          <Shop category={category} />
         </Route>
         <Route path="/profile">
           Profile
--- src/components/shop/drop-down/DropDown.jsx
+++ src/components/shop/drop-down/DropDown.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import "./DropDown.css";
-
-export default function DropDown() {
+import { AiFillCaretUp } from "react-icons/ai";
+export default function DropDown({ changeCategory }) {
   return (
-    <div>
-
+    <div className="drop-down-container">
+      <span>Select</span>
+      <AiFillCaretUp className="categories-arrow-down" />
+      <ul className="categories-checkbox-container">
+        <li>
+          <label htmlFor="all">All</label>
+          <input value="" type="radio" name="category" id="all" onClick={(e) => changeCategory(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="men-clothing">Men Clothing</label>
+          <input value="men clothing" type="radio" name="category" id="men-clothing" onClick={(e) => changeCategory(e.target.value)} />
+        </li>
+        <li>
+          <label htmlFor="men-clothing">electronics</label>
+          <input onClick={(e) => changeCategory(e.target.value)} value="electronics" type="radio" name="category" id="electronics" />
+        </li>
+        <li>
+          <label htmlFor="men-clothing">jewellery</label>
+          <input onClick={(e) => changeCategory(e.target.value)} value="jewelery" type="radio" name="category" id="jewellery" />
+        </li>
+        <li>
+          <label htmlFor="men-clothing">women clothing</label>
+          <input onClick={(e) => changeCategory(e.target.value)} value="women clothing" type="radio" name="category" id="women-clothing" />
+        </li>
+      </ul>
     </div>
   )
 }
